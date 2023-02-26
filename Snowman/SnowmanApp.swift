a165ef31505656036f857d1f6d70f27f4b35f1c8
Jackson Evarts <jack.evarts@gmail.com>
2023-02-26 15:51:03 -0500

    Initial Commit

---
+++ Snowman/SnowmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowmanApp.swift
+//  Snowman
+//
+//  Created by Jack Evarts on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
