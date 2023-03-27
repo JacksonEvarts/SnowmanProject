f89894f850fde12e8d34a5e314d1819bb40998e9
tccreigh <126419765+tccreigh@users.noreply.github.com>
2023-03-27 00:02:15 -0400

    Update ContentView.swift

--- Snowman/ContentView.swift
+++ Snowman/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 
 struct ContentView: View {
-    var wordList = "dictionary.txt"
-    var ans = "FROSTY"
+    @State var ans: String 
     let letters = Array("ABCDEFGHIJKLMNOPQRSTUVWXYZ'")
     @State var win = false
     @State var loss = false
@@ -19,49 +18,59 @@ struct ContentView: View {
     @State var uncAns: String // Initialized as string of "_" but will slowly become the correct word
     @State var buttonDisabledStates: [Bool] = Array(repeating: false, count: 27) // Initializes array of disabled states for each button
     init() {
+        var str1 = ""
+        if let path = Bundle.main.path(forResource: "dictionary", ofType: "txt"){
+            do{
+                let contents = try String(contentsOfFile: path, encoding: .utf8)
+                let lines = contents.split(separator:"\n")
+                str1 = String(lines.randomElement() ?? "")
+            } catch {
+                
+            }
+        }
         var str = ""
-        for _ in 0..<ans.count {
+        for _ in 0..<str1.count {
             str += "_ "
         }
         self.uncAns = str
+        self.ans = str1.uppercased()
     } // Postcondition: Initializes uncAns into a string with as many _ as there are letters in the final answer
-    
     var body: some View {
         ZStack{
             Image("Scene").resizable().ignoresSafeArea().blur(radius: 3.0)
             VStack{
                 Spacer()
                 VStack{ // For snowman
-                     if loss == true {
-                         Button("You Lost! - Click to Restart") {
-                             loss = false
-                             numWrong = 0
-                             uncAnsPos = []
-                             buttonDisabledStates = Array(repeating: false, count: 27)
-                             //ans = newWord(wordList)
-                         }
-                     } else if win == true {
-                         Button("You Win! - Click to Restart") {
-                             win = false
-                             numWrong = 0
-                             buttonDisabledStates = Array(repeating: false, count: 27)
-                             //ans = newWord(wordList)
-                         }
-                     } else {
-                         Button("Click to Restart") {
-                             numWrong = 0
-                             buttonDisabledStates = Array(repeating: false, count: 27)
-                             //ans = newWord(wordList)
-                         }
-                     }
-                     Image("Tophat").resizable().scaledToFit().aspectRatio(0.70, contentMode: .fit).opacity(numWrong > 5 ? 1.0 : 0.0)
-                     Image("Snowball").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).opacity(numWrong > 4 ? 1.0 : 0.0)
-                     HStack{
-                         Image("Stick").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).rotationEffect(Angle(degrees: 200), anchor: .center).opacity(numWrong > 2 ? 1.0 : 0.0)
-                         Image("Snowball").resizable().scaledToFit().aspectRatio(0.90, contentMode: .fit).opacity(numWrong > 1 ? 1.0 : 0.0)
-                         Image("Stick").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).rotationEffect(Angle(degrees:350), anchor: .center).opacity(numWrong > 3 ? 1.0 : 0.0)
-                     }
-                     Image("Snowball").resizable().scaledToFit().aspectRatio(0.70, contentMode: .fit).opacity(numWrong > 0 ? 1.0 : 0.0)
+                    if loss == true {
+                        Button("You Lost! - Click to Restart") {
+                            loss = false
+                            numWrong = 0
+                            uncAnsPos = []
+                            buttonDisabledStates = Array(repeating: false, count: 27)
+                            newWord()
+                        }
+                    } else if win == true {
+                        Button("You Win! - Click to Restart") {
+                            win = false
+                            numWrong = 0
+                            buttonDisabledStates = Array(repeating: false, count: 27)
+                            newWord()
+                        }
+                    } else {
+                        Button("Click to Restart") {
+                            numWrong = 0
+                            buttonDisabledStates = Array(repeating: false, count: 27)
+                            newWord()
+                        }
+                    }
+                    Image("Tophat").resizable().scaledToFit().aspectRatio(0.70, contentMode: .fit).opacity(numWrong > 5 ? 1.0 : 0.0)
+                    Image("Snowball").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).opacity(numWrong > 4 ? 1.0 : 0.0)
+                    HStack{
+                        Image("Stick").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).rotationEffect(Angle(degrees: 200), anchor: .center).opacity(numWrong > 2 ? 1.0 : 0.0)
+                        Image("Snowball").resizable().scaledToFit().aspectRatio(0.90, contentMode: .fit).opacity(numWrong > 1 ? 1.0 : 0.0)
+                        Image("Stick").resizable().scaledToFit().aspectRatio(0.50, contentMode: .fit).rotationEffect(Angle(degrees:350), anchor: .center).opacity(numWrong > 3 ? 1.0 : 0.0)
+                    }
+                    Image("Snowball").resizable().scaledToFit().aspectRatio(0.70, contentMode: .fit).opacity(numWrong > 0 ? 1.0 : 0.0)
                 }
                 Spacer()
                 HStack{ //For ans
@@ -83,7 +92,7 @@ struct ContentView: View {
                                             if numWrong > 5 {
                                                 loss = true
                                                 buttonDisabledStates = Array(repeating: true, count: 27)
-                                                }
+                                            }
                                         }
                                         uncAns = ansLine(uncAnsPositions: uncAnsPos, uncAnswers: uncAns, answer: ans)
                                         win = checkWin(uncAns: uncAns)
@@ -101,6 +110,24 @@ struct ContentView: View {
             }
         }
     }
+    func newWord() { // Precondition: dictionary.txt is a file within Assets
+        var str1 = ""
+        if let path = Bundle.main.path(forResource: "dictionary", ofType: "txt"){
+            do{
+                let contents = try String(contentsOfFile: path, encoding: .utf8)
+                let lines = contents.split(separator:"\n")
+                str1 = String(lines.randomElement() ?? "")
+            } catch {
+                
+            }
+        }
+        ans = str1.uppercased()
+        var str = ""
+        for _ in 0..<str1.count {
+            str += "_ "
+        }
+        uncAns = str
+    }  // Postcondition: returns a new word from the dictionary
 }
 
 // Precondition: Function is passed a valid letter and word.
@@ -117,20 +144,20 @@ func guess(letter: Character, word: String) -> [Int] {
 // Function takes array of positions of guessed values within answer, a string showing blank spaces and previously uncovered letters, and an answer string.
 func ansLine(uncAnsPositions: [Int], uncAnswers: String, answer: String) -> String {
     var result = ""
-        var index = 0
-        for char in uncAnswers {
-            if char != " " {
-                if uncAnsPositions.contains(index) {
-                    result.append(answer[answer.index(answer.startIndex, offsetBy: index)])
-                } else {
-                    result.append(char)
-                }
-                index += 1
+    var index = 0
+    for char in uncAnswers {
+        if char != " " {
+            if uncAnsPositions.contains(index) {
+                result.append(answer[answer.index(answer.startIndex, offsetBy: index)])
             } else {
                 result.append(char)
             }
+            index += 1
+        } else {
+            result.append(char)
         }
-        return result
+    }
+    return result
 }
 // Returns updated string of blank spaces (underscores) and uncovered letters in approprate positions.
 
@@ -144,20 +171,8 @@ func checkWin(uncAns: String) -> Bool {
     return true
 } // Postcondition: Returns false if there are still letters to guess (there are underscores), true if there are none left and the user wins
 
-/*
-func newWord() -> String { // Precondition: dictionary.txt is a file within Assets
-    if let path = Bundle.main.path(forResource: "dictionary", ofType: "txt") {
-        do {
-            let words = try String(contentsOfFile: path, encoding: .utf8) // Creating
-        } catch {
-            print("Error reading file: \(error)")
-        }
-    }
-    
-    //TODO: Select a random line of the string "words"
 
-} // Postcondition: returns a new word from the dictionary
-*/
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
